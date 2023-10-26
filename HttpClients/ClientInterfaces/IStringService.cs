e287eefbea5f07bb57f0bd49b237ab109c45f2cb
KBence <kabaly.bence@gmail.com>
2023-10-26 16:27:03 +0200

    set up and blazor

---
+++ HttpClients/ClientInterfaces/IStringService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HttpClients.ClientInterfaces
+{
+    public interface IStringService
+    {
+        Task CreateAsync(string dto);
+    
+        Task<ICollection<string>> GetAsync();
+    }
+}
---
+++ HttpClients/Implementations/StringHttpClient.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Net.Http.Json;
+using System.Text.Json;
+using HttpClients.ClientInterfaces;
+
+namespace HttpClients.Implementations
+{
+    public class StringHttpClient : IStringService
+    {
+        private readonly HttpClient client;
+
+        public StringHttpClient(HttpClient client)
+        {
+            this.client = client;
+        }
+        
+        public async Task CreateAsync(string dto)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync("/string",dto);
+            if (!response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                throw new Exception(content);
+            }
+        }
+
+        public async Task<ICollection<string>> GetAsync()
+        {
+            HttpResponseMessage response = await client.GetAsync("/post");
+            string content = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception(content);
+            }
+
+            ICollection<string> posts = JsonSerializer.Deserialize<ICollection<string>>(content, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            })!;
+            return posts;
+        }
+    }
+}
