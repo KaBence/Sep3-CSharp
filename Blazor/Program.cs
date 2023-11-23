67ad90df67f66483b5e8e0a0d3e662c6c2b25ae5
Flolol0o <116642272+Flolol0o@users.noreply.github.com>
2023-11-23 21:39:40 +0100

    cleaning after proof of concept

--- Blazor/Program.cs
+++ Blazor/Program.cs
@@ -1,7 +1,4 @@
 using Blazor.Auth;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Blazor.Data;
 using Blazor.Services;
 using Blazor.Services.Http;
 using HttpClients.ClientInterfaces;
@@ -14,8 +11,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddScoped<IStringService, StringHttpClient>();
 builder.Services.AddScoped<IUserService, UserHttpClient>();
 builder.Services.AddScoped<IAuthService, JwtAuthService>();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthProvider>();
