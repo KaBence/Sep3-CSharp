54a7bd426ec19b29c3b8daebdbd93f6b95618797
KBence <kabaly.bence@gmail.com>
2023-11-27 15:06:38 +0100

    empty constructor for SearchProductDto

--- Shared/DTOs/Search/SearchProductDto.cs
+++ Shared/DTOs/Search/SearchProductDto.cs
@@ -6,6 +6,10 @@ public class SearchProductDto
     public double? Price { get; set; }
     public double? Amount { get; set; }
 
+    public SearchProductDto()
+    {
+    }
+
     public SearchProductDto(string? type, double? price, double? amount)
     {
         Type = type;
