304f1702990994b0180f246d6bf17c73961beb0d
Flolol0o <116642272+Flolol0o@users.noreply.github.com>
2023-12-01 15:34:59 +0100

    all reviews by farmer

---
+++ HttpClients/Implementations/ReviewHttpClient.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+using HttpClients.ClientInterfaces;
+using Shared.DTOs.Create;
+using Shared.Models;
+
+namespace HttpClients.Implementations;
+
+public class ReviewHttpClient:IReviewServices
+{
+    private readonly HttpClient client;
+
+    public ReviewHttpClient(HttpClient client)
+    {
+        this.client = client;
+    }
+
+    public Task<string> CreateReview(ReviewCreateDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> PostComment(CommentCreateDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Review>> GetReviewsByFarmer(string farmer)
+    {
+        HttpResponseMessage response = await client.GetAsync($"/Review/{farmer}");
+        string content = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(content);
+        }
+
+        ICollection<Review> reviews = JsonSerializer.Deserialize<ICollection<Review>>(content, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return reviews;
+    }
+}
